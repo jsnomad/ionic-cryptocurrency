051649190f7223114c914a7834edad0a27526b79
jsnomad <thomas.blanc.hector@gmail.com>
2018-01-28 15:45:53 +0700

    change price color

--- src/components/coin-card/coin-card.ts
+++ src/components/coin-card/coin-card.ts
@@ -9,4 +9,17 @@ export class CoinCardComponent {
   @Input() coinData: any;
 
   constructor(private imageHelper: ImageHelper) {}
+
+  private getPriceColor(price: number) {
+    switch (Math.sign(price)) {
+      case 1:
+        return 'price-up';
+      case -1:
+        return 'price-down';
+      case 0:
+        return 'price-no-change';
+      default:
+        return 'price-no-change';
+    }
+  }
 }
