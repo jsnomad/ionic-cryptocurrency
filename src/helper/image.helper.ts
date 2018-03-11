114c9e99d9f3ebeb222ffc5db2f0dd03d29960a2
jsnomad <thomas.blanc.hector@gmail.com>
2018-03-11 18:40:22 +0700

    fix image path

--- src/helper/image.helper.ts
+++ src/helper/image.helper.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ImageHelper {
-  private readonly BASE_IMAGE_PATH: string = 'https://files.coinmarketcap.com/static/img/coins/32x32/';
+  private readonly BASE_IMAGE_PATH: string = 'https://raw.githubusercontent.com/cjdowner/cryptocurrency-icons/master/32/icon/';
   private readonly DEFAULT_EXTENSION: string = 'png';
 
   constructor() {}
 
   public getCoinImage(coin: string): string {
-    return `${this.BASE_IMAGE_PATH}${coin}.${this.DEFAULT_EXTENSION}`;
+    return `${this.BASE_IMAGE_PATH}${coin.toLowerCase()}.${
+      this.DEFAULT_EXTENSION
+    }`;
   }
 }
